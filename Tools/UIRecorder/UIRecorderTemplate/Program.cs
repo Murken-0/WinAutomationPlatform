6370966201647f7ea0f5cc0646172b4503e61735
Murken <muravievvlad.2002@gmail.com>
2024-04-10 15:25:10 +0300

    Remove unnececary

--- Tools/UIRecorder/UIRecorderTemplate/Program.cs
+++ Tools/UIRecorder/UIRecorderTemplate/Program.cs
@@ -1,20 +1,4 @@
-﻿//******************************************************************************
-//
-// Copyright (c) 2019 Microsoft Corporation. All rights reserved.
-//
-// This code is licensed under the MIT License (MIT).
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-//******************************************************************************
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,80 +10,76 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UIXPathLib
 {
-    public class DesktopSession
-    {
-        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723/";
-        WindowsDriver<WindowsElement> desktopSession;
+	public class DesktopSession
+	{
+		private const string WindowsApplicationDriverUrl = "http://localhost:4723/";
+		WindowsDriver<WindowsElement> desktopSession;
 
-        public DesktopSession()
-        {
-            DesiredCapabilities appCapabilities = new DesiredCapabilities();
-            appCapabilities.SetCapability("app", "Root");
-            appCapabilities.SetCapability("deviceName", "WindowsPC");
-            desktopSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
-        }
+		public DesktopSession()
+		{
+			DesiredCapabilities appCapabilities = new DesiredCapabilities();
+			appCapabilities.SetCapability("app", "Root");
+			appCapabilities.SetCapability("deviceName", "WindowsPC");
+			desktopSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+		}
 
-        ~DesktopSession()
-        {
-            desktopSession.Quit();
-        }
+		~DesktopSession()
+		{
+			desktopSession.Quit();
+		}
 
-        public WindowsDriver<WindowsElement> DesktopSessionElement
-        {
-            get { return desktopSession; }
-        }
+		public WindowsDriver<WindowsElement> DesktopSessionElement
+		{
+			get { return desktopSession; }
+		}
 
-        public WindowsElement FindElementByAbsoluteXPath(string xPath, int nTryCount = 10)
-        {
-            WindowsElement uiTarget = null;
+		public WindowsElement FindElementByAbsoluteXPath(string xPath, int nTryCount = 10)
+		{
+			WindowsElement uiTarget = null;
 
-            while (nTryCount-- > 0)
-            {
-                try
-                {
-                    uiTarget = desktopSession.FindElementByXPath(xPath);
-                }
-                catch
-                {
-                }
+			while (nTryCount-- > 0)
+			{
+				try
+				{
+					uiTarget = desktopSession.FindElementByXPath(xPath);
+				}
+				catch
+				{
+				}
 
-                if (uiTarget != null)
-                {
-                    break;
-                }
-                else
-                {
-                    System.Threading.Thread.Sleep(2000);
-                }
-            }
+				if (uiTarget != null)
+				{
+					break;
+				}
+				else
+				{
+					System.Threading.Thread.Sleep(2000);
+				}
+			}
 
-            return uiTarget;
-        }
-    }
+			return uiTarget;
+		}
+	}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DesktopSession desktopSession = new DesktopSession();
-            System.Threading.Thread.Sleep(2000);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DesktopSession desktopSession = new DesktopSession();
+			System.Threading.Thread.Sleep(2000);
 
-            bool bSuccess = false;
+			bool bSuccess = false;
 
-            try
-            {
-                //Paste generated code here
+			try
+			{
+				//Enter c# code here
 
-
-
-
-                //test complete
-                bSuccess = true;
-            }
-            finally
-            {
-                Assert.AreEqual(bSuccess, true);
-            }
-        }
-    }
+				bSuccess = true;
+			}
+			finally
+			{
+				Assert.AreEqual(bSuccess, true);
+			}
+		}
+	}
 }
